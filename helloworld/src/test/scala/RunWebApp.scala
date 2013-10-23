06d8bb9916d55f1978514ef5c1f7954e9455c556
Yorrick Jansen <yjansen@auto123.com>
2013-10-22 20:07:30 -0400

    updated to lift 2.5

--- helloworld/src/test/scala/RunWebApp.scala
+++ helloworld/src/test/scala/RunWebApp.scala
@@ -1,15 +1,21 @@
-import _root_.org.mortbay.jetty.Connector
-import _root_.org.mortbay.jetty.Server
-import _root_.org.mortbay.jetty.webapp.WebAppContext
+import org.eclipse.jetty.server.handler.ContextHandler
+import org.eclipse.jetty.server.nio.SelectChannelConnector
+import org.eclipse.jetty.server.{Handler, Server}
+import org.eclipse.jetty.webapp.WebAppContext
 
-object RunWebApp extends Application {
-  val server = new Server(8080)
+object RunWebApp extends App {
+  val server = new Server
+  val scc = new SelectChannelConnector
+  scc.setPort(8080)
+  server.setConnectors(Array(scc))
+
   val context = new WebAppContext()
   context.setServer(server)
-  context.setContextPath("/")
   context.setWar("src/main/webapp")
 
-  server.addHandler(context)
+  val context0: ContextHandler = new ContextHandler()
+  context0.setHandler(context)
+  server.setHandler(context0)
 
   try {
     println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
@@ -20,7 +26,7 @@ object RunWebApp extends Application {
     server.stop()
     server.join()
   } catch {
-    case exc : Exception => {
+    case exc: Exception => {
       exc.printStackTrace()
       System.exit(100)
     }
