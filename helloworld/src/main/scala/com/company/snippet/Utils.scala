93100a36fcf1bff4ba5c52b511f41b2a0b30a36a
Yorrick Jansen <yjansen@auto123.com>
2013-11-04 17:51:59 -0500

    much simpler css handling

--- helloworld/src/main/scala/com/company/snippet/Utils.scala
+++ helloworld/src/main/scala/com/company/snippet/Utils.scala
@@ -2,31 +2,19 @@ package com.company.snippet
 
 import scala.xml.{Null, NodeSeq, Text, Attribute}
 import net.liftweb.util._
+import net.liftweb.util.Helpers._
 
 
-class Utils {
+object Utils {
 
-  def includeLessFile(linkNode: NodeSeq) = {
-      if (Props.getBool("lessProdMode", true)) {
-        linkNode
-      } else {
-        val lessCompiler = <script id="less-compiler" src="/js/external/less.js" type="text/javascript"></script>
-        val href = (linkNode \ "@href").toString
+  def manageHead = {
+    val classesToRemove = Props.get("cssMode", "dev") match {
+      case "dev" => ".prodCss"
+      case "prod" => ".devCss"
+      case _ => ".devCss"
+    }
 
-        val newLinkNone = href.split('/').toList match {
-          case "":: "css" :: remainingPath if remainingPath.last.endsWith(".css") =>
-            val path = remainingPath.dropRight(1)
-            val cssFileName = remainingPath.last
-            val lessFilename = cssFileName.replace(".css", ".less")
-            val newPath = "/less/" + path.mkString("/") + lessFilename
-
-            (<link rel="stylesheet/less" type="text/css" media="screen, projection"/>  % Attribute(None, "href", Text(newPath), Null))
-              .union(lessCompiler)
-          case _ => linkNode
-        }
-
-        newLinkNone
-      }
+    classesToRemove #> NodeSeq.Empty
   }
 }
 
