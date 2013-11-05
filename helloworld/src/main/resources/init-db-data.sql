95bf07659f9fd3d900532972ebc4e2a63f6691c9
Yorrick Jansen <yjansen@auto123.com>
2013-11-05 16:55:23 -0500

    css js

--- helloworld/src/main/resources/init-db-data.sql
+++ helloworld/src/main/resources/init-db-data.sql
@@ -1,19 +1,19 @@
 BEGIN;
 
-ALTER SEQUENCE used_vehicle_id_seq RESTART WITH 1000;
+    DELETE FROM used_vehicle;
 
-INSERT INTO used_vehicle
-SELECT 11, 77, 'text one'
-WHERE NOT EXISTS (
-    SELECT id FROM used_vehicle WHERE id = 11
-);
+    INSERT INTO used_vehicle
+    SELECT 11, 77, 'text one'
+    WHERE NOT EXISTS (
+        SELECT id FROM used_vehicle WHERE id = 11
+    );
 
-INSERT INTO used_vehicle
-SELECT 22, 88, 'text two'
-WHERE NOT EXISTS (
-    SELECT id FROM used_vehicle WHERE id = 22
-);
+    INSERT INTO used_vehicle
+    SELECT 22, 88, 'text two'
+    WHERE NOT EXISTS (
+        SELECT id FROM used_vehicle WHERE id = 22
+    );
 
-
+    ALTER SEQUENCE used_vehicle_id_seq RESTART WITH 1000;
 
 END;
